ee9aefbfb1762d121df4f46315c59fcedbfb572d
zhangyaguang1 <zhangyaguang1@360.cn>
2021-11-25 11:23:05 +0800

    feat: replace favicon and adjust vuepress config

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// base: '/github-pages-vuepress/',
+	title: 'VuePress',
+	description: 'Static website built with VuePress and deployed on Github Pages',
+	dest: './docs',
+    head: [
+        [
+            'link', 
+            { 
+                rel: 'icon', 
+                href: './../favicon.ico' 
+            }
+        ]
+    ]
+};
