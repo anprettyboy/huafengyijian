f0b3d8dd21bda429b30a18dba6703cb962adece7
zhangyaguang1 <zhangyaguang1@360.cn>
2021-11-25 14:34:16 +0800

    feat

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-
+npm i
 # 生成静态文件
 npm run docs:build
 
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.huafengyijian.top' > CNAME
+echo 'www.huafengyijian.top' > CNAME
 
 git init
 git add -A
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:anprettyboy/huafengyijian.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:anprettyboy/huafengyijian.git main:gh-pages
+# git push -f git@github.com:anprettyboy/huafengyijian.git main:gh-pages
 
 cd -
