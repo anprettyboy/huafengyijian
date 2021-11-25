e28149d19d2895973b94e768cf63fdd9dcafadb8
zhangyaguang1 <zhangyaguang1@360.cn>
2021-11-25 11:17:21 +0800

    feat: adjust deploy shell

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+echo 'www.huafengyijian.top' > CNAME
 
 git init
 git add -A
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:anprettyboy/huafengyijian.git main
+# git push -f git@github.com:anprettyboy/huafengyijian.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
